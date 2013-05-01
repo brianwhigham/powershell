d068c7dc1e28e7ddcf5b90303101a0fd4715ac04
Brian Whigham <brian@usg.edu>
2013-05-01 15:40:25 -0400

    example connection to given server, and running given command

--- winrm.rb
+++ winrm.rb
@@ -1,15 +1,11 @@
 #!/usr/bin/ruby1.9.1
 require 'winrm'
-#print "#{ARGV[0]}"
 endpoint = "http://#{ARGV[0]}:5985/wsman"
-#krb5_realm = 'uso.bor.usg.edu'
-krb5_realm = 'USO.BOR.USG.EDU'
+krb5_realm = File.open("realm.conf").read
 winrm = WinRM::WinRMWebService.new(endpoint, :kerberos, :realm => krb5_realm)
-#winrm = WinRM::WinRMWebService.new(endpoint, :ssl, :user => 'brian', :pass => 'asdf', :disable_sspi => true)
-#winrm = WinRM::WinRMWebService.new(endpoint, :plaintext, :user => 'brian', :pass => 'asdf', :disable_sspi => true)
-print "#{ARGV[1]}\n"
-exit
+#winrm = WinRM::WinRMWebService.new(endpoint, :ssl, :user => 'myuser', :pass => 'asdf', :disable_sspi => true)
+#winrm = WinRM::WinRMWebService.new(endpoint, :plaintext, :user => 'myuser', :pass => 'asdf', :disable_sspi => true)
 winrm.cmd("#{ARGV[1]}") do |stdout, stderr|
-#  STDOUT.print stdout
-#  STDERR.print stderr
+  STDOUT.print stdout
+  STDERR.print stderr
 end
